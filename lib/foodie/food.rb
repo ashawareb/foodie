6880a51db91955d44b66fe811e9ffa9326c0efd9
ashawareb <bzsc1010@gmail.com>
2022-10-09 11:58:53 +0200

    implement pluralize method

--- lib/foodie/food.rb
+++ lib/foodie/food.rb
@@ -1,10 +1,18 @@
+require 'active_support/inflector'
+
 module Foodie
   class Food
-    def self.portray(food)
-      if food.downcase.eql?('broccoli')
-        'Gross!'
-      else
-        'Delicious!'
+    class << self
+      def portray(food)
+        if food.downcase.eql?('broccoli')
+          'Gross!'
+        else
+          'Delicious!'
+        end
+      end
+
+      def pluralize(word)
+        word.pluralize
       end
     end
   end
